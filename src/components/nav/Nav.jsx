a41ea96d157deb2fb04d9cc4b49ecedcda503f6a
Muhammad Abdullah <mabdullah.dev1@gmail.com>
2024-11-12 00:54:43 +0500

    :tada: FINISH: finish nav component (updated colors of the website)

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,7 +1,52 @@
 import "./nav.css";
+import { IoHomeOutline } from "react-icons/io5";
+import { FaRegUser } from "react-icons/fa";
+import { BsBriefcase } from "react-icons/bs";
+import { LiaToolsSolid } from "react-icons/lia";
+import { BiMessageDetail } from "react-icons/bi";
+import { useState } from "react";
 
 const Nav = () => {
-  return <div>Nav</div>;
+  const [activeNav, setActiveNav] = useState("#header");
+  return (
+    <nav>
+      <a
+        href="#header"
+        onClick={() => setActiveNav("#header")}
+        className={activeNav === "#header" ? "active" : ""}
+      >
+        <IoHomeOutline />
+      </a>
+      <a
+        href="#about"
+        onClick={() => setActiveNav("#about")}
+        className={activeNav === "#about" ? "active" : ""}
+      >
+        <FaRegUser />
+      </a>
+      <a
+        href="#experience"
+        onClick={() => setActiveNav("#experience")}
+        className={activeNav === "#experience" ? "active" : ""}
+      >
+        <BsBriefcase />
+      </a>
+      <a
+        href="#services"
+        onClick={() => setActiveNav("#services")}
+        className={activeNav === "#services" ? "active" : ""}
+      >
+        <LiaToolsSolid />
+      </a>
+      <a
+        href="#contact"
+        onClick={() => setActiveNav("#contact")}
+        className={activeNav === "#contact" ? "active" : ""}
+      >
+        <BiMessageDetail />
+      </a>
+    </nav>
+  );
 };
 
 export default Nav;
