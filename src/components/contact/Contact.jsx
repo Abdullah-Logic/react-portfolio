3cb75f9c9a715b6b71f6326b31bb8a3a5be6901c
Muhammad Abdullah <mabdullah.dev1@gmail.com>
2024-11-12 23:37:37 +0500

    :tada: FINISH: finish contact component

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,7 +1,78 @@
 import "./contact.css";
+import { useRef } from "react";
+import emailjs from "emailjs-com";
+import { MdOutlineEmail } from "react-icons/md";
+import { FaWhatsapp } from "react-icons/fa";
 
 const Contact = () => {
-  return <section id="contact">Contact</section>;
+  const form = useRef();
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm(
+      "service_xl5gfql",
+      "template_tzp8p2l",
+      form.current,
+      "bnaEOCs8DyJqzH7Ab"
+    );
+    e.target.reset();
+  };
+  return (
+    <section id="contact">
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <MdOutlineEmail className="contact__option-icon" />
+            <h4>Email</h4>
+            <h5>abdullah.dev1@gmail.com</h5>
+            <a
+              href="mailto:abdullah.dev1@gmail.com"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Send a message
+            </a>
+          </article>
+
+          <article className="contact__option">
+            <FaWhatsapp className="contact__option-icon" />
+            <h4>Whatsapp</h4>
+            <h5>+92 331 559 0956</h5>
+            <a
+              href="https://api.whatsapp.com/send?phone=923315590956"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Send a message
+            </a>
+          </article>
+        </div>
+        {/* END OF OPTIONS */}
+
+        <form ref={form} onSubmit={sendEmail}>
+          <input
+            type="text"
+            name="name"
+            placeholder="Your Full Name"
+            required
+          />
+          <input type="email" name="email" placeholder="Your Email" required />
+          <textarea
+            name="message"
+            rows="7"
+            placeholder="Your Message"
+            required
+          ></textarea>
+          <button type="submit" className="btn btn-primary">
+            Send Message
+          </button>
+        </form>
+      </div>
+    </section>
+  );
 };
 
 export default Contact;
