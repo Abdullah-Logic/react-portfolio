75e4070d768465e0bf1fe3034b366f45bddee11a
Muhammad Abdullah <mabdullah.dev1@gmail.com>
2024-11-22 14:46:34 +0500

    :sparkles: CODE-IMPROVEMENT: 1-asset-management

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,50 +1,50 @@
 import "./portfolio.css";
 import {
-  portfolio1,
-  portfolio2,
-  portfolio3,
-  portfolio4,
-  portfolio5,
-  portfolio6,
+  biakitchen,
+  landingpage,
+  techdoc,
+  tribute,
+  responsivebakery,
+  portfolio,
 } from "../../assets/image";
 
 const data = [
   {
     id: 1,
-    image: portfolio1,
+    image: biakitchen,
     title: "Bia's Baking Kitchen",
     github: "https://github.com/Abdullah-dev1/bias-baking-kitchen.git",
   },
   {
     id: 2,
-    image: portfolio2,
+    image: landingpage,
     title: "Virtual Bakery",
     github: "https://github.com/Abdullah-dev1/bakery-responsive.git",
   },
   {
     id: 3,
-    image: portfolio3,
+    image: techdoc,
     title: "Product Landing Page",
     github:
       "https://github.com/Abdullah-dev1/legacy-responsive-web-design/tree/c99a86f17f6233884a9d4f17550a2e7941981edf/landing_page",
   },
   {
     id: 4,
-    image: portfolio4,
+    image: tribute,
     title: "Technical Documentation Page",
     github:
       "https://github.com/Abdullah-dev1/legacy-responsive-web-design/tree/c99a86f17f6233884a9d4f17550a2e7941981edf/technical_doc_page",
   },
   {
     id: 5,
-    image: portfolio5,
+    image: responsivebakery,
     title: "Tribute Page",
     github:
       "https://github.com/Abdullah-dev1/legacy-responsive-web-design/tree/c99a86f17f6233884a9d4f17550a2e7941981edf/tribute_page",
   },
   {
     id: 6,
-    image: portfolio6,
+    image: portfolio,
     title: "Simple Portfolio Page",
     github:
       "https://github.com/Abdullah-dev1/legacy-responsive-web-design/tree/c99a86f17f6233884a9d4f17550a2e7941981edf/personal_port_page",
